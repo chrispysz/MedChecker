3b1ed04b6de8d43f1fdda891918ed59dc24b689a
Krzysztof Pysz <krzysztof.pysz99@gmail.com>
2021-05-01 21:40:12 +0200

    Began work on camera detection

--- app/src/main/java/com/example/cachingapp/ui/CameraFragment.kt
+++ app/src/main/java/com/example/cachingapp/ui/CameraFragment.kt
@@ -1,9 +1,32 @@
 package com.example.cachingapp.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cachingapp.R
+import com.example.cachingapp.databinding.FragmentCameraBinding
+import com.example.cachingapp.databinding.FragmentDebunkDetailsBinding
 
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
